91ca8ad5a999f04e837ffb29324e74fcbb6a2977
Gonzalo Martinez <gonzalo.martinez@litermi.com>
2025-04-11 03:49:49 -0300

    Disabled LSP

--- init.lua
+++ init.lua
@@ -1,40 +1,4 @@
 require("plugins")
 require("Config.LoadConfig")
 
--- LSP configuration for gopls
-local lspconfig = require('lspconfig')
-
-lspconfig.gopls.setup({
-    on_attach = function(client, bufnr)
-        local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
-        local opts = { noremap=true, silent=true }
-
-        -- Key mappings
-        buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
-        buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
-        buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-        buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-        buf_set_keymap('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-        buf_set_keymap('n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-    end,
-    settings = {
-        gopls = {
-            analyses = {
-                unusedparams = true,
-            },
-            staticcheck = true,
-        },
-    },
-})
-
-
-
-
-
-
-
-
-
-
-
 
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -44,7 +44,7 @@ return packer.startup(function(use)
 
     -- LSP 
     
-    use 'neovim/nvim-lspconfig'
+    -- use 'neovim/nvim-lspconfig'
 
     -- Themes 
     use 'Mofiqul/vscode.nvim'
