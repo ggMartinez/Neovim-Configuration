284964752b17fac3902104fd205837da1af71178
Gonzalo Martinez <gonzalo.martinez@litermi.com>
2025-08-07 03:49:07 -0300

    TREE FOCUS!!!!

--- lua/Config/Plugins/NvimTreeToggle.lua
+++ lua/Config/Plugins/NvimTreeToggle.lua
@@ -1,4 +1,6 @@
 vim.api.nvim_set_keymap('n', '<S-T>', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<S-R>', ':NvimTreeFocus<CR>', { noremap = true, silent = true })
+
 require("nvim-tree").setup({
     git = {
         enable = true,
